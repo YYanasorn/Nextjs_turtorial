b04720c640d5f6652a3000aaa8cbfd5f87213612
YYanasorn <susu.yana7@gmail.com>
2025-04-01 23:05:52 +0700

    "setting drizzle and better auth"

---
+++ advanced-nextjs-app/src/lib/auth.ts
@@ -0,0 +1,12 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import conn from "@/db"; // your drizzle instance
+
+const db = await conn;
+
+ 
+export const auth = betterAuth({
+    database: drizzleAdapter(db, {
+        provider: "mysql", // or "mysql", "sqlite"
+    })
+});
